7f785ac67d8c7ca981a0b04373ca0d5e050091ab
Xavier Lepretre <xavier.lepretre@gmail.com>
2020-04-02 15:50:20 +0400

    Added parameters to ExternalContent component.

--- src/ExternalContent.js
+++ src/ExternalContent.js
@@ -1,43 +1,56 @@
 import React from "react"
 //import Prism from "prismjs";
 
-
 export default class ExternalContent extends React.Component {
+    /**
+     * @param props = {
+     *     url: {string}
+     *     lineStart: {number}, the first line to include. Enumeration starts at 1.
+     *     lineEnd: {number}, the last line to include. Enumeration starts at 1.
+     *     codeType: {string}, recognised language name, e.g. "java"
+     * }
+     */
     constructor(props) {
         super(props)
         this.state = {
-            url: 'https://raw.githubusercontent.com/corda/samples/release-V4/cordapp-example/workflows-java/src/main/java/com/example/flow/ExampleFlow.java',
+            url: props.url,
+            lineStart: props.lineStart,
+            lineEnd: props.lineEnd,
+            codeType: props.codeType,
             content: ""
         }
     }
 
     componentDidMount() {
-        this.loadData(this.state.url);
+        const { url, lineStart, lineEnd } = this.state;
+        this.loadData(url, lineStart, lineEnd);
     }
 
-    loadData = (url) => {
-        fetch(url)
+    loadData = (url, lineStart, lineEnd) => {
+        return fetch(url)
             .then(function (response) {
                 return response.text();
             })
             .then(function (data) {
-                this.setState({ content: data })
+                this.setState({
+                    content: data.slice(lineStart - 1, lineEnd).join('\n')
+                });
                 //Prism.highlightAll();
             }.bind(this))
             .catch(function (err) {
-                console.log("Error loading url: ", url, err.stack);
+                console.error("Error loading url: ", url, err.stack);
             });
     }
 
     render() {
         return (
             <pre>
-                <code className='java'>
+                <code className='{this.state.codeType}'>
                     {this.state.content}
                 </code>
             </pre>
         )
     }
-} 
+}
 
 //<div dangerouslySetInnerHTML={{__html: this.state.content}}/>
