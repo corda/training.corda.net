ab70446b04223457008bc24c14e74bc3c052c0a4
Ibo Sy <ibo@b9lab.com>
2020-05-11 13:03:25 +0200

    enable latex support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
-import "./src/css/layout.css"
-import "./src/css/poppins.css"
-import "./src/css/prism-ghcolors.css"
-import "./src/css/override.css"
+import "./src/css/layout.css";
+import "./src/css/poppins.css";
+import "./src/css/prism-ghcolors.css";
+import "./src/css/override.css";
+require(`katex/dist/katex.min.css`);
